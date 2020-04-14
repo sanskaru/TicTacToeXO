ff2fb85ed65898c48028a065d394146934d6f26e
sanskaru <hrsan420884@gmail.com>
2020-04-14 21:00:15 +0530

    Bug fixed: Option to restart game in the case of a draw added.

--- app/src/main/java/com/s090/tictactoexo/MainActivity.java
+++ app/src/main/java/com/s090/tictactoexo/MainActivity.java
@@ -17,10 +17,11 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
     int playerState=0; // at present at two player local mode. 0 is red and 1 is yellow.
     int[] boardState={-1,-1,-1,-1,-1,-1,-1,-1,-1}; // -1 means unplayed, else stores playerState, denoting which player tapped which cell
-    public void congrats(String msg) // method to call dialog box
+    int playCounter = 0; // denotes no grid has been tapped yet
+    public void myDialog(String title, String msg) // method to call dialog box
     {
         AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-        builder.setTitle("Congratulations!").setMessage(msg).setCancelable(false);
+        builder.setTitle(title).setMessage(msg).setCancelable(false);
         builder.setPositiveButton("Play Again", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
@@ -43,52 +44,60 @@ public class MainActivity extends AppCompatActivity {
 
                 playerState = 1;
                 counter.setImageResource(R.drawable.red);
+                playCounter++;
             } else {
                 playerState = 0;
                 counter.setImageResource(R.drawable.yellow);
+                playCounter++;
             }
             counter.animate().translationYBy(1000f).rotation(360f).setDuration(300);
             boardState[cellState] = playerState;
         }   //all possible winning positions
             if(boardState[0]==boardState[1] && boardState[1]==boardState[2] && boardState[0]!=-1) {
                 Log.i("Info","Player "+boardState[0]+" has won.");
-                congrats("Player "+boardState[0]+" has won.");
+                myDialog("Congratulations!","Player "+boardState[0]+" has won.");
             }
             else if(boardState[0]==boardState[3] && boardState[3]==boardState[6] && boardState[0]!=-1)
             {
                 Log.i("Info","Player "+boardState[0]+" has won.");
-                congrats("Player "+boardState[0]+" has won.");
+                myDialog("Congratulations!","Player "+boardState[0]+" has won.");
             }
             else if(boardState[0]==boardState[4] && boardState[4]==boardState[8] && boardState[0]!=-1)
             {
                 Log.i("Info","Player "+boardState[0]+" has won.");
-                congrats("Player "+boardState[0]+" has won.");
+                myDialog("Congratulations!","Player "+boardState[0]+" has won.");
             }
             else if(boardState[1]==boardState[4] && boardState[4]==boardState[7] && boardState[1]!=-1)
             {
                 Log.i("Info","Player "+boardState[1]+" has won.");
-                congrats("Player "+boardState[1]+" has won.");
+                myDialog("Congratulations!","Player "+boardState[1]+" has won.");
             }
             else if(boardState[2]==boardState[5] && boardState[5]==boardState[8] && boardState[2]!=-1)
             {
                 Log.i("Info","Player "+boardState[2]+" has won.");
-                congrats("Player "+boardState[2]+" has won.");
+                myDialog("Congratulations!","Player "+boardState[2]+" has won.");
             }
             else if(boardState[2]==boardState[4] && boardState[4]==boardState[6] && boardState[2]!=-1)
             {
                 Log.i("Info","Player "+boardState[2]+" has won.");
-                congrats("Player "+boardState[2]+" has won.");
+                myDialog("Congratulations!","Player "+boardState[2]+" has won.");
             }
             else if(boardState[3]==boardState[4] && boardState[4]==boardState[5] && boardState[3]!=-1)
             {
                 Log.i("Info","Player "+boardState[3]+" has won.");
-                congrats("Player "+boardState[3]+" has won.");
+                myDialog("Congratulations!","Player "+boardState[3]+" has won.");
             }
             else if(boardState[6]==boardState[7] && boardState[7]==boardState[8] && boardState[6]!=-1)
             {
                 Log.i("Info","Player "+boardState[6]+" has won.");
-                congrats("Player "+boardState[6]+" has won.");
+                myDialog("Congratulations!","Player "+boardState[6]+" has won.");
             }
+            if (playCounter==9)
+            {
+                Log.i("Info","Board full.");
+                myDialog("It's a Draw!", "The board is full. Please play again.");
+            }
+
     }
 
     @Override
